3727b51b5cf4c8ddcbc6dad62beaa06873187c14
ppoojaprasad <poojaprasadpeddinti@gmail.com>
2016-12-05 10:34:18 -0800

    Update ipaddr.py

--- ipaddr.py
+++ ipaddr.py
@@ -1,29 +1,30 @@
 from netaddr import *
 import sys
 import re
-#def validIPforScanning(range,exclude_list):
-range = '192.128.1.0/24'
-set = ['192.128.1.17','192.128.1.4','172.32.113.1']
+def validIPforScanning(range,exclude_list):
+#range = '192.128.1.0/24'
+#set = ['192.128.1.17','192.128.1.4','172.32.113.1']
+# regex to check if the network range is valid or not
 valid = re.match( r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[/]([1-9]$|[1-2][0-9]$|3[0-2])$',range)
-
 if(not valid):
   print("Invalid address range input")
-  sys.exit()
+  return()
+# list of IP addresses in the network range
 r= IPSet([IPNetwork(range)])  
+# error check for valid ip address in the given set and also if the ip addresses in the set are in the given range or not
 for i in set:
   if(not valid_ipv4(i)):
     print("Invalid IP addresses in the input set")
-    sys.exit()
-    #return() 
+    return() 
   if(i not in r):
     print("Input set contains values not in the given address range")
     sys.exit()
     
-
 rangeiplist=[]
 outputlist=[]
 for ip in r:
   rangeiplist.append(ip)
+#removing a IP address not required to use in the scanning tool
 for i in set:
   if IPAddress(i) in rangeiplist:
     print("Removing the ip address")
@@ -32,4 +33,4 @@ for i in set:
 for ip in r:
   outputlist.append(ip)
   
-print(outputlist)
+return(outputlist)
